dd930b3818387f26bf60e4e1bbff6171977b404e
pivovarVARA <71059902+pivovarVARA@users.noreply.github.com>
2020-11-03 14:46:38 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,37 +1,75 @@
-import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+ ciphertext = ""
+ m = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ z = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+ while shift >= 26:
+     shift -= 26
+ for i in range(len(plaintext)):
+  l = 0
+  for j in range(26):
+   if plaintext[i] == m[j]:
+    l = 1
+  for j in range(26):
+   if plaintext[i] == z[j]:
+    l = 1
+  if l == 0:
+   ciphertext += plaintext[i]
+  else:
+         if plaintext[i].islower():
+             index = m.index(plaintext[i])
+             if index + shift < 26:
+                index += shift
+                ciphertext += m[index]
+             else:
+                index = index + shift - 26
+                ciphertext += m[index]      
+         else:
+                index = z.index(plaintext[i])
+                if index + shift < 26:
+                  index += shift
+                  ciphertext += z[index]
+                else:
+                   index = index + shift - 26
+                   ciphertext += z[index]
+
+        
+ return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
+ 
+ plaintext = ""
+ m = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ z = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+ while shift >= 26:
+     shift -= 26
+ for i in range(len(ciphertext)):
+    l = 0
+  for j in range(26):
+   if palintext[i] == m[j]:
+    l = 1
+  for j in range(26):
+   if palintext[i] == z[j]:
+    l = 1
+  if l == 0:
+   ciphertext += plaintext[i]
+  else:
+             if ciphertext[i].islower():
+                index = m.index(ciphertext[i])
+                if index - shift >= 0:
+                    index -= shift
+                    plaintext += m[index]
+                else:
+                    index = index - shift+26
+                    plaintext += m[index]      
+             else:
+                 index = z.index(ciphertext[i])
+                 if index - shift >= 0:
+                    index -= shift
+                    plaintext += z[index]
+                 else:
+                   index = index - shift + 26
+                   plaintext += z[index]
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+ return plaintext
